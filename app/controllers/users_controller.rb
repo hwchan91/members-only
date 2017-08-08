99f2eba01cea5a6fa04f4632d60761bf344c8161
hwchan91 <hwchan91@gmail.com>
2017-08-08 17:23:45 +0800

    add create user and post

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User created"
+      sign_in @user
+      redirect_to posts_path
+    else
+      flash[:danger] = "Error"
+      render 'new'
+    end
+  end
+
+  private
+   def user_params
+     params.require(:user).permit(:username, :email, :password,
+                                  :password_confirmation)
+   end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
   get  '/login',  to: 'sessions#new'
   post '/login',  to: 'sessions#create'
   delete  '/logout', to: 'sessions#delete'
   resources :posts,     only: [:new, :create, :index]
+  resources :users, only: [:new, :create]
 end
